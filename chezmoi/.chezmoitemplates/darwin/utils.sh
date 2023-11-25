4b8d264a8064499bfcb1d063c52d7d40881608ec
Ilya Pavlov <piv@devmem.ru>
2023-11-25 12:23:57 +0300

    initial macos support

---
+++ chezmoi/.chezmoitemplates/darwin/utils.sh
@@ -0,0 +1,27 @@
+function filename {
+  echo "🟠 $1"
+}
+
+function group {
+  echo "📦 $1"
+}
+
+function prompt {
+  echo "💬 $1"
+}
+
+function pad {
+  echo -n "   "
+}
+
+function action {
+  echo "🚀 $1"
+}
+
+function ignored {
+  echo "✨ $1"
+}
+
+function error {
+  echo "🛑 $1" >&2
+}
